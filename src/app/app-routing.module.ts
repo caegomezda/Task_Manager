a3e04e13cf9bfe2b79a6784be7c035e4a3dc3b2e
Edugoda314 <camiloedg23@gmail.com>
2023-04-24 16:02:05 -0700

    ubication change in the pages path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
@@ -13,43 +13,43 @@ const routes: Routes = [
   },
   {
     path: 'sign-in',
-    loadChildren: () => import('./sign-in/sign-in.module').then( m => m.SignInPageModule)
+    loadChildren: () => import('./pages/sign-in/sign-in.module').then( m => m.SignInPageModule)
   },
   {
     path: 'sign-up',
-    loadChildren: () => import('./sign-up/sign-up.module').then( m => m.SignUpPageModule)
+    loadChildren: () => import('./pages/sign-up/sign-up.module').then( m => m.SignUpPageModule)
   },
   {
     path: 'email-verifier',
-    loadChildren: () => import('./email-verifier/email-verifier.module').then( m => m.EmailVerifierPageModule)
+    loadChildren: () => import('./pages/email-verifier/email-verifier.module').then( m => m.EmailVerifierPageModule)
   },
   {
     path: 'project-data-first-step',
-    loadChildren: () => import('./project-data-first-step/project-data-first-step.module').then( m => m.ProjectDataFirstStepPageModule)
+    loadChildren: () => import('./pages/project-data-first-step/project-data-first-step.module').then( m => m.ProjectDataFirstStepPageModule)
   },
   {
     path: 'project-data-second-step',
-    loadChildren: () => import('./project-data-second-step/project-data-second-step.module').then( m => m.ProjectDataSecondStepPageModule)
+    loadChildren: () => import('./pages/project-data-second-step/project-data-second-step.module').then( m => m.ProjectDataSecondStepPageModule)
   },
   {
     path: 'project-data-third-step',
-    loadChildren: () => import('./project-data-third-step/project-data-third-step.module').then( m => m.ProjectDataThirdStepPageModule)
+    loadChildren: () => import('./pages/project-data-third-step/project-data-third-step.module').then( m => m.ProjectDataThirdStepPageModule)
   },
   {
     path: 'project-data-fourth-step',
-    loadChildren: () => import('./project-data-fourth-step/project-data-fourth-step.module').then( m => m.ProjectDataFourthStepPageModule)
+    loadChildren: () => import('./pages/project-data-fourth-step/project-data-fourth-step.module').then( m => m.ProjectDataFourthStepPageModule)
   },
   {
     path: 'project-data-fifth-step',
-    loadChildren: () => import('./project-data-fifth-step/project-data-fifth-step.module').then( m => m.ProjectDataFifthStepPageModule)
+    loadChildren: () => import('./pages/project-data-fifth-step/project-data-fifth-step.module').then( m => m.ProjectDataFifthStepPageModule)
   },
   {
     path: 'project-data-visualized',
-    loadChildren: () => import('./project-data-visualized/project-data-visualized.module').then( m => m.ProjectDataVisualizedPageModule)
+    loadChildren: () => import('./pages/project-data-visualized/project-data-visualized.module').then( m => m.ProjectDataVisualizedPageModule)
   },
   {
     path: 'sign-up',
-    loadChildren: () => import('./sign-up/sign-up.module').then( m => m.SignUpPageModule)
+    loadChildren: () => import('./pages/sign-up/sign-up.module').then( m => m.SignUpPageModule)
   },
 ];
 
